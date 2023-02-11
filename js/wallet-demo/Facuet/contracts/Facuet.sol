823894bf1bd03a13257cff83e8a55eefb8a2de63
godotc <disyourself@qq.com>
2023-02-11 22:47:41 +0800

    add facuet contract

---
+++ js/wallet-demo/Facuet/contracts/Facuet.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
+
+contract owned {
+    address owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(
+            msg.sender == owner,
+            "only the contract owner can call this function"
+        );
+        _;
+    }
+}
