4cb197002e24cd534a923272cda6626c5eac00e2
godotc <disyourself@qq.com>
2023-03-26 02:14:15 +0800

    gstl add stash files

--- cc/gstl/debug.h
+++ cc/gstl/debug.h
@@ -1,11 +1,39 @@
 #pragma once
 
 #include <cstdarg>
+#include <cstdint>
+#include <cstdio>
 #include <iostream>
+#include <string>
 
-#ifdef DEBUG
-#define LOG(...)                                \
-    do {                                        \
-        std::cout << __VAR_ARGS__ << std::endl; \
+
+enum class DEBUG_LEVEL : uint8_t
+{
+    DEBUG = 0,
+    TRACE,
+    INFO,
+    WARN,
+    ERROR,
+    FATAL
+};
+
+constexpr DEBUG_LEVEL LOG_LEVEL = DEBUG_LEVEL::DEBUG;
+
+
+#define LOG(LEVEL, ...)                                                 \
+    do {                                                                \
+        if (LEVEL >= LOG_LEVEL) {                                       \
+            const char *format = "[%s] %s\n";                           \
+            auto        len    = sprintf(format, ##LEVEL, __VA_ARGS__); \
+            char        msg[len];                                       \
+            printf(msg, format, ##LEVEL, __VA_ARGS__);                  \
+            std::cout << msg << std::endl;                              \
+        }                                                               \
     } while (0)
-#endif
+
+#define DBG(...) LOG(DEBUG_LEVEL::WARN, __VA_ARGS__)
+
+
+static void test()
+{
+}
--- cc/gstl/main.cc
+++ cc/gstl/main.cc
@@ -12,7 +12,11 @@ int main()
     std::cout << a << std::endl;
 
 
-    v.push_back({1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+    v.push_back({1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11});
+
+    for (int i = 0; i < 11; ++i) {
+        std::cout << v.pop_back() << std::endl;
+    }
 
     return 0;
 }
