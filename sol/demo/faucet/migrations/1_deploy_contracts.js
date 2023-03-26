dd102411fd3f6ca2f8563c9c55742ac4bc0052e0
godotc <disyourself@qq.com>
2023-03-26 14:20:33 +0800

    rm old files

--- sol/demo/faucet/migrations/1_deploy_contracts.js
+++
@@ -1,6 +0,0 @@
-
-var Faucet = artfacts.require('Faucet')
-
-module.exports = function (deployer) {
-	deployer.deploy(Faucet, 600, '0x3df186851464ad9dc8f5142cb29d30f24a38c8695399a8f43b5a327729e9d982');
-}
