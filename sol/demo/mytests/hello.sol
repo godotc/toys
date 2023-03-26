f484616a0b7b836f430ff1d8b8c3b84ceca094f7
godotc <disyourself@qq.com>
2023-03-26 14:20:33 +0800

    rm old files

--- sol/demo/mytests/hello.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
-
-contract Hello{
-    function hi() public pure returns (string memory){
-        return "Hello ethereum!";
-    }
-}
--- sol/transfer_test/migrations/1_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-const CIVIL3 = artifacts.require("CIVIL3")
-
-module.exports = function(deployer){
-	deployer.deploy(CIVIL3,"CIVIL3", "CIVIL",50, 18);
-};
