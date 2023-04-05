0d7b93ce69a4f9bc23e9e0674996fc5b2f080e0d
godotc <disyourself@qq.com>
2023-04-05 22:57:33 +0800

    fill kecck256 encode call

--- block/test/contracts/encode.sol
+++ block/test/contracts/encode.sol
@@ -6,6 +6,21 @@ contract A {
         (bool success, ) = address_.call(abi.encodeWithSignature("bFunction(uint256,string)", num_, message_));
         return success;
     }
+
+    function callBFunction__sig(address address_, uint256 num_, string memory message_) public returns (bool) {
+        bytes4 sig = bytes4(keccak256("bFunction(uint256,string)"));
+        bytes memory _num = abi.encode(num_);
+        bytes memory _message = abi.encode(message_);
+
+        (bool success, ) = address_.call(abi.encodePacked(sig, _num, _message));
+        return success;
+    }
+
+    function callBFunction__selsector(address address_, uint256 num_, string memory message_) public returns (bool) {
+        bytes4 sig = bytes4(keccak256("bFunction(uint256,string)"));
+        (bool success, ) = address_.call(abi.encodeWithSelector(sig, num_, message_));
+        return success;
+    }
 }
 
 contract B {
