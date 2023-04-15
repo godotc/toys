5c6728a3fd979b345593bead31584ba9cf07d5ba
godotc <disyourself@qq.com>
2023-04-16 03:57:18 +0800

    Add customer test framework
    - qemu use iso-basexxxx mode and with -display none to boot
    - use serial to print message from inherit to host computer's console
    - #[cfg(not(test))] conditional compilation to overload the panic function in test mode
    - add exit qemu function from 0x04, and uart_16550 framework
    
    TODO:
    times out section.it's 2023-4-16 3:56:41.
    I'm going to get a sleep, +8

--- os/rcore/ros_2nd_EDITION/src/main.rs
+++ os/rcore/ros_2nd_EDITION/src/main.rs
@@ -1,25 +1,73 @@
 #![no_std]
 #![no_main]
+// customor test framework
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"] // because we has #![no_main] attribute
 
+mod serial;
 mod vga_buffer;
 
 use core::panic::PanicInfo;
-
-// contain file,line... panic msg
-// -> ! means defined as a diverging function: function will never return `never type`
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    println!("{}", _info);
-    loop {}
-}
-
-static HELLO: &[u8] = b"Hello World!";
 
 // disable name mangling: output func with the name _start without attribute
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello world {} !", "godot42");
 
-    panic!("some msg");
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
+
+// qemu isa-debug-exit exit function for io read/wirt on x86_64 arch
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    serial_print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    //assert_eq!(0, 1);
+    serial_println!("[ok]");
+}
+
+// contain file,line... panic msg
+// -> ! means defined as a diverging function: function will never return `never type`
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
+    loop {}
+}
+
+// for serial test
+#[cfg(test)]
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", _info);
+    exit_qemu(QemuExitCode::Failed);
     loop {}
 }
