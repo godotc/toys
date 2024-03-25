b44ca48b98772c6bea59b64a285fa06c8f8832ca
chirs z <disyourself@qq.com>
2024-03-25 11:11:39 +0800

    rock n roll!

--- cc/scripts/reflect/function_traits.hpp
+++ cc/scripts/reflect/function_traits.hpp
@@ -2,7 +2,6 @@
 #include <stdint.h>
 #include <tuple>
 #include <type_traits>
-#include <vcruntime.h>
 
 
 namespace detail {
@@ -21,15 +20,15 @@ struct basic_function_traits<Ret(Args...)> {
 
 
 
-template <class FuncSig>
+template <typename Func>
 struct function_traits;
 
 template <typename Ret, typename... Args>
 struct function_traits<Ret(Args...)>
     : detail::basic_function_traits<Ret(Args...)> {
     using type                      = Ret(Args...);
-    using args_with_class           = std::tuple<Args...>; // remove class in declaration?
-    using pointer                   = Ret (*)(Args...);    // function ptr
+    using args_with_class           = std::tuple<Args...>;
+    using pointer                   = Ret (*)(Args...);
     static constexpr bool is_member = false;
     static constexpr bool is_const  = false;
 };
@@ -39,7 +38,7 @@ struct function_traits<Ret (Class::*)(Args...)>
     : detail::basic_function_traits<Ret(Args...)> {
     using type                      = Ret (Class::*)(Args...);
     using args_with_class           = std::tuple<Class *, Args...>;
-    using pointer                   = Ret (Class::*)(Args...); // function ptr
+    using pointer                   = Ret (Class::*)(Args...);
     static constexpr bool is_member = true;
     static constexpr bool is_const  = false;
 };
@@ -47,9 +46,9 @@ struct function_traits<Ret (Class::*)(Args...)>
 template <typename Ret, typename Class, typename... Args>
 struct function_traits<Ret (Class::*)(Args...) const>
     : detail::basic_function_traits<Ret(Args...)> {
-    using type                      = Ret (Class::*)(Args...);
+    using type                      = Ret (Class::*)(Args...) const;
     using args_with_class           = std::tuple<Class *, Args...>;
-    using pointer                   = Ret (Class::*)(Args...); // function ptr
+    using pointer                   = Ret (Class::*)(Args...) const;
     static constexpr bool is_member = true;
     static constexpr bool is_const  = true;
 };
