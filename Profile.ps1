c1be24b9d105998f699f9a0a2711c22517c10ce9
godot42 <disyourself@qq.com>
2024-07-12 20:14:55 +0800

    profiles

--- Profile.ps1
+++ Profile.ps1
@@ -31,6 +31,41 @@ function Add-DirectoryToPath {
     }
 }
 
+function Kill-All {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$SubString,
+
+        [switch]$Force
+    )
+
+    # Get all processes with names containing the specified substring
+    $processes = Get-Process | Where-Object { $_.Name -like "*$SubString*" }
+
+    # Check if there are any processes found
+    if ($processes) {
+        # List the processes with more information
+        Write-Host "Found the following processes:"
+        $counter = 1
+        @($processes) | foreach { "[$counter] Name: $($_.Name) - PID: $($_.Id) - Description: $($_.Description)" ; $counter++ }
+
+        # Ask the user to confirm whether they want to kill the processes
+        $confirmation = if ($Force) { 'Y' } else { Read-Host "Do you want to kill these processes? (Y/N)" }
+
+        if ($confirmation -eq 'Y') {
+            # Kill the processes
+            foreach ($process in $processes) {
+                Write-Host "Killing process: $($process.Name) - PID: $($process.Id)"
+                Stop-Process -InputObject $process -Force
+            }
+        } else {
+            Write-Host "Process killing cancelled."
+        }
+    } else {
+        Write-Host "No processes found with the specified substring: $SubString"
+    }
+}
+
 #region conda initialize
 # !! Contents within this block are managed by 'conda init' !!
 If (Test-Path "C:\ProgramData\miniconda3\Scripts\conda.exe") {
